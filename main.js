3eedd75ac1bfef093dee8040ba086053e88e948f
AbundioMartinez <60721671+AbundioMartinez@users.noreply.github.com>
2020-05-20 18:57:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,543 @@
+
+const canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+//left shoe
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(0, 762, 20, 20);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(20, 762, 20, 20);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(40, 762, 20, 20);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(60, 762, 20, 20);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(20, 742, 20, 20);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(40, 742, 20, 20);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(60, 742, 20, 20);
+
+//right shoe
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(160, 762, 20, 20);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(180, 762, 20, 20);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(200, 762, 20, 20);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(220, 762, 20, 20);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(160, 742, 20, 20);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(180, 742, 20, 20);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(200, 742, 20, 20);
+
+//overol
+
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(40, 722, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(60, 722, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(80, 722, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(40, 702, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(60, 702, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(80, 702, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(60, 682, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(80, 682, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(60, 662, 20, 20);
+
+ctx.fillStyle = "#ffff00";
+ctx.fillRect(80, 662, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(80, 642, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(80, 622, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(80, 602, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(100, 702, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(120, 702, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(100, 682, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(120, 682, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(100, 662, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(120, 662, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(100, 642, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(120, 642, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(140, 722, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(160, 722, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(180, 722, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(140, 702, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(160, 702, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(180, 702, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(140, 682, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(160, 682, 20, 20);
+
+ctx.fillStyle = "#ffff00";
+ctx.fillRect(140, 662, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(160, 662, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(140, 642, 20, 20);
+
+ctx.fillStyle = "#0099ff";
+ctx.fillRect(140, 622, 20, 20);
+
+//shirt
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(100, 622, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(120, 622, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(100, 602, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(120, 602, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(140, 602, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(160, 602, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(160, 622, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(180, 622, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(200, 622, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(160, 642, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(180, 642, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(200, 642, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(220, 642, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(60, 642, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(60, 622, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(60, 602, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(40, 642, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(40, 622, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(40, 602, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(40, 662, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(20, 642, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(20, 622, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(0, 642, 20, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(180, 662, 20, 20);
+
+//hands
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(40, 682, 20, 20);
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(20, 662, 20, 20);
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(0, 662, 20, 20);
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(0, 682, 40, 40);
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(180, 682, 20, 20);
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(200, 682, 40, 40);
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(200, 662, 20, 20);
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(220, 662, 20, 20);
+
+//face
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(100, 502, 60, 100);
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(60, 562, 40, 40);
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(80, 522, 20, 20);
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(40, 522, 20, 40);
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(160, 582, 60, 20);
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(200, 542, 60, 20);
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(180, 522, 60, 20);
+
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(180, 502, 20, 20);
+
+ctx.fillStyle = "#ffdead";
+ctx.fillRect(160, 542, 20, 20);
+
+//hair
+
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(20, 562, 40, 20);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(20, 522, 20, 40);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(40, 502, 60, 20);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(60, 522, 20, 40);
+
+ctx.fillStyle = "#a0522d";
+ctx.fillRect(80, 542, 20, 20);
+
+//hat
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(40, 482, 200, 20);
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(60, 462, 120, 20);
+ 
+//eye
+
+ctx.fillStyle = "black";
+ctx.fillRect(160, 502, 20, 40);
+
+ctx.fillStyle = "black";
+ctx.fillRect(160, 562, 80, 20);
+
+ctx.fillStyle = "black";
+ctx.fillRect(180, 542, 20, 20);
+
+//tanque
+
+ctx.fillStyle = "#008000";
+ctx.fillRect(700, 632, 200, 150);
+
+ctx.fillStyle = "#008000";
+ctx.fillRect(675, 632, 250, 50);
+
+ctx.fillStyle = "#f2f2f2";
+ctx.fillRect(850, 632, 20, 150);
+
+ctx.fillStyle = "#333333";
+ctx.fillRect(700, 682, 200, 20);
+
+//square
+
+ctx.fillStyle = "#ffcc00";
+ctx.fillRect(0, 350, 90, 90);
+
+ctx.fillStyle = "#331a00";
+ctx.fillRect(90, 350, 90, 90);
+
+ctx.fillStyle = "#ffcc00";
+ctx.fillRect(180, 350, 90, 90);
+
+ctx.fillStyle = "#e6b800";
+ctx.fillRect(10, 360, 10, 10);
+
+ctx.fillStyle = "#e6b800";
+ctx.fillRect(10, 420, 10, 10);
+
+ctx.fillStyle = "#e6b800";
+ctx.fillRect(70, 360, 10, 10);
+
+ctx.fillStyle = "#e6b800";
+ctx.fillRect(70, 420, 10, 10);
+
+ctx.fillStyle = "#e6b800";
+ctx.fillRect(190, 360, 10, 10);
+
+ctx.fillStyle = "#e6b800";
+ctx.fillRect(190, 420, 10, 10);
+
+ctx.fillStyle = "#e6b800";
+ctx.fillRect(250, 360, 10, 10);
+
+ctx.fillStyle = "#e6b800";
+ctx.fillRect(250, 420, 10, 10);
+
+//bricks
+
+ctx.fillStyle = "#804000";
+ctx.fillRect(90, 350, 40, 25);
+
+ctx.fillStyle = "#804000";
+ctx.fillRect(140, 350, 40, 25);
+
+ctx.fillStyle = "#804000";
+ctx.fillRect(90, 415, 40, 25);
+
+ctx.fillStyle = "#804000";
+ctx.fillRect(140, 415, 40, 25);
+
+ctx.fillStyle = "#804000";
+ctx.fillRect(115, 383, 40, 25);
+
+ctx.fillStyle = "#804000";
+ctx.fillRect(90, 383, 20, 25);
+
+ctx.fillStyle = "#804000";
+ctx.fillRect(160, 383, 20, 25);
+
+//? left
+
+ctx.fillStyle = "white";
+ctx.fillRect(35, 370, 20, 10);
+
+ctx.fillStyle = "white";
+ctx.fillRect(55, 370, 10, 30);
+
+ctx.fillStyle = "white";
+ctx.fillRect(45, 390, 20, 10);
+
+ctx.fillStyle = "white";
+ctx.fillRect(45, 390, 10, 20);
+
+ctx.fillStyle = "white";
+ctx.fillRect(45, 420, 10, 10);
+
+ctx.fillStyle = "white";
+ctx.fillRect(30, 370, 10, 20);
+
+//? right
+
+ctx.fillStyle = "white";
+ctx.fillRect(215, 370, 20, 10);
+
+ctx.fillStyle = "white";
+ctx.fillRect(235, 370, 10, 30);
+
+ctx.fillStyle = "white";
+ctx.fillRect(225, 390, 20, 10);
+
+ctx.fillStyle = "white";
+ctx.fillRect(225, 390, 10, 20);
+
+ctx.fillStyle = "white";
+ctx.fillRect(225, 420, 10, 10);
+
+ctx.fillStyle = "white";
+ctx.fillRect(210, 370, 10, 20);
+
+
+//left cloud
+
+ctx.fillStyle = "white";
+ctx.fillRect(200, 60, 100, 20);
+
+ctx.fillStyle = "white";
+ctx.fillRect(170, 20, 200, 40);
+
+ctx.fillStyle = "white";
+ctx.fillRect(190, 0, 150, 40);
+
+ctx.fillStyle = "#d9d9d9";
+ctx.fillRect(170, 60, 30, 20);
+
+ctx.fillStyle = "#d9d9d9";
+ctx.fillRect(300, 60, 70, 20);
+
+ctx.fillStyle = "#d9d9d9";
+ctx.fillRect(200, 80, 100, 20);
+
+//sinlge 
+
+ctx.fillStyle = "#ffcc00";
+ctx.fillRect(90, 10, 90, 90);
+
+ctx.fillStyle = "#e6b800";
+ctx.fillRect(100, 80, 10, 10);
+
+ctx.fillStyle = "#e6b800";
+ctx.fillRect(100, 20, 10, 10);
+
+ctx.fillStyle = "#e6b800";
+ctx.fillRect(160, 80, 10, 10);
+
+ctx.fillStyle = "#e6b800";
+ctx.fillRect(160, 20, 10, 10);
+
+//? single
+
+ctx.fillStyle = "white";
+ctx.fillRect(125, 30, 20, 10);
+
+ctx.fillStyle = "white";
+ctx.fillRect(145, 30, 10, 30);
+
+ctx.fillStyle = "white";
+ctx.fillRect(135, 50, 20, 10);
+
+ctx.fillStyle = "white";
+ctx.fillRect(135, 50, 10, 20);
+
+ctx.fillStyle = "white";
+ctx.fillRect(135, 80, 10, 10);
+
+ctx.fillStyle = "white";
+ctx.fillRect(120, 30, 10, 20);
+
+// right cloud
+
+ctx.fillStyle = "white";
+ctx.fillRect(600, 100, 200, 20);
+
+ctx.fillStyle = "white";
+ctx.fillRect(630, 80, 200, 20);
+
+ctx.fillStyle = "white";
+ctx.fillRect(660, 60, 200, 20);
+
+ctx.fillStyle = "white";
+ctx.fillRect(630, 120, 100, 20);
+
+ctx.fillStyle = "#d9d9d9";
+ctx.fillRect(630, 140, 100, 20);
+
+ctx.fillStyle = "#d9d9d9";
+ctx.fillRect(730, 120, 70, 20);
+
+ctx.fillStyle = "#d9d9d9";
+ctx.fillRect(800, 100, 30, 20);
+
+ctx.fillStyle = "#d9d9d9";
+ctx.fillRect(830, 80, 30, 20);
+
+ctx.fillStyle = "#d9d9d9";
+ctx.fillRect(600, 120, 30, 20);
+
+
+// shrubbery
+
+ctx.fillStyle = "#33cc33";
+ctx.fillRect(300, 762, 300, 20);
+
+ctx.fillStyle = "#33cc33";
+ctx.fillRect(325, 722, 300, 40);
+
+ctx.fillStyle = "#33cc33";
+ctx.fillRect(365, 682, 240, 40);
+
+ctx.fillStyle = "#33cc33";
+ctx.fillRect(395, 642, 170, 40);
+
+ctx.fillStyle = "#33cc33";
+ctx.fillRect(420, 622, 100, 20);
+
+ctx.fillStyle = "#33cc33";
+ctx.fillRect(440, 602, 50, 20);
+
+ctx.fillStyle = "#404040";
+ctx.fillRect(385, 682, 60, 20);
+
+ctx.fillStyle = "#404040";
+ctx.fillRect(495, 722, 60, 20);
+
